48d3940a74c50a1140f18d29906d89ec731d3a14
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-07-18 16:34:53 -0300

    fix: shadcn config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Button } from '../components/ui';
 export default function Home() {
   return (
     <main>
       <h1>NextJS</h1>
+      <Button variant="default">Primary</Button>
     </main>
   );
 }
